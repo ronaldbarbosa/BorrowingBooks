f1f18b1621cec13d8c63f8a84897c6fc5c9c8d96
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-06-05 00:38:06 -0300

    Created ASP.NET Core MVC App

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BorrowingBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
