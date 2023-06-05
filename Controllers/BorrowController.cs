f0eaceb487a73e99a375436fc16a7ea5bd7fde4a
Ronald Barbosa <59707505+ronaldbarbosa@users.noreply.github.com>
2023-06-05 00:49:06 -0300

    Edited controller and views to get and render db data

---
+++ Controllers/BorrowController.cs
@@ -0,0 +1,20 @@
+﻿using BorrowingBooks.Data;
+using BorrowingBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BorrowingBooks.Controllers
+{
+    public class BorrowController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+        public BorrowController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<BorrowModel> borrowList = _db.Borrowings;
+            return View(borrowList);
+        }
+    }
+}
